5baec48766cadfacb6e925074f679cbb093b4768
ilyass anida <anidailyass@gmail.com>
2025-01-01 13:23:18 +0100

    #61 Get the submited data in the signup page.

--- app/Core/BasePage.php
+++ app/Core/BasePage.php
@@ -18,12 +18,17 @@
             }
 
             $path = APPROOT . "View" . $path . ".php";
+
+            $role = "client";
+            if (isLoggedIn()) {
+                $role = "admin";
+            }
             
             if (file_exists($path)) {
                 extract($data);
-                require_once APPROOT . "View/components/header.php";
+                require_once APPROOT . "View/". $role ."/components/header.php";
                 require_once $path;
-                require_once APPROOT . "View/components/footer.php";
+                require_once APPROOT . "View/". $role ."/components/footer.php";
             } else {
                 http_response_code(404);
                 echo "404 Not Found";
--- public/index.php
+++ public/index.php
@@ -11,11 +11,16 @@
     require_all_files(__DIR__ . '/../app/Core');
     $db = new Database();
     BaseClass::setDatabase($db);
-    
+
+
+    require_once __DIR__ . '/../app/Helpers/url_helper.php';
+    require_once __DIR__ . '/../app/Helpers/session_helper.php';
+
     // Define the routes
     $router = new Router();
     $request = new Request();
 
-    $router->add('GET', '/', 'LoginPage@index');
+    $router->add('GET', '/signup', 'SignupPage@index');
+    $router->add('POST', '/signup', 'SignupPage@signup');
 
     $router->dispatch($request);
