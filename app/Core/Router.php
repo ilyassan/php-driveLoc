fbe76fd46469ff427cea20c3b44f2c2d443fe651
ilyass anida <anidailyass@gmail.com>
2025-01-01 12:32:46 +0100

    #56 Create the routing system logic.

---
+++ app/Core/Router.php
@@ -0,0 +1,53 @@
+<?php
+
+class Router
+{
+    private $routes = [];
+
+    public function add($method, $path, $callback)
+    {
+        $this->routes[] = compact('method', 'path', 'callback');
+    }
+
+    public function dispatch($request)
+    {
+        foreach ($this->routes as $route) {
+            $isRouteAcceptParam = strpos($route['path'], "/{id}") != false;
+            $param = null;
+            if ($isRouteAcceptParam) {
+                $route['path'] = str_replace('/{id}', '', $route['path']);
+                $param = str_replace($route['path'], '', $request->getPath());
+            }
+
+            $requestPath = $param ? str_replace($param, '', $request->getPath()) : $request->getPath();
+            if ($route['method'] === $request->getMethod() &&
+                $route['path'] === $requestPath) {
+                
+                if (is_callable($route['callback'])) {
+                    return call_user_func($route['callback']);
+                }
+                
+                if (is_string($route['callback'])) {
+                    [$page, $action] = explode('@', $route['callback']);
+                    
+                    // Require the page file
+                    $pageFile = __DIR__ . '/../Pages/' . $page . '.php';
+            
+                    if (file_exists($pageFile)) {
+                        require_once $pageFile;
+                        
+                        // Instantiate the page and call the action
+                        $pageInstance = new $page();
+                        return $param != null ? $pageInstance->$action(str_replace("/", "", $param)) : $pageInstance->$action();
+                    } else {
+                        http_response_code(500);
+                        echo "Page file not found: $pageFile";
+                        return;
+                    }
+                }
+            }
+        }
+
+        redirect('');
+    }
+}
